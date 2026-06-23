8beb10aaa1deb137282bc9e2362c4b1d08ad57f0
Aaryan Soni <aaryansoni577@gmail.com>
2026-06-23 16:51:08 -0500

    messing with THREE.js and fixed testimonial links

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,35 @@
 // import { app as firebase } from '../firebase-config.js';
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+// const controls = new OrbitControls( camera, renderer.domElement );
+// const loader = new GLTFLoader();
+
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//
+// const mainCanvas = document.getElementById('main-canvas');
+// const renderer = new THREE.WebGLRenderer({canvas: mainCanvas, antialias: true});
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild( renderer.domElement );
+//
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+//
+// camera.position.z = 5;
+//
+// function animate( time ) {
+//
+//   cube.rotation.x += 0.001;
+//   cube.rotation.y += 0.001;
+//
+//   renderer.render( scene, camera );
+// }
+// renderer.setAnimationLoop( animate );
 
 
 //handling dropdowns
@@ -72,10 +103,10 @@ testimonialLogos.forEach(logo => {
   logo.addEventListener('click', () => {
     //use dataset to determine href
     try {
-      window.open(logo.dataset.href, '_blank');
+      window.open(logo.firstElementChild.dataset.href, '_blank');
     } catch (e) {
       console.log(e)
-      location.href = logo.dataset.href;
+      location.href = logo.firstElementChild.dataset.href;
     }
   })
 })
